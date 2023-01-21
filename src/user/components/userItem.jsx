896991a1371e5edb4b0cb2dca2187ee0316ff060
eng-mostafa-sayed <ms519197@gmail.com>
2023-01-21 15:26:22 +0200

    arch

---
+++ src/user/components/userItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../css/user/components/UserItem.css";
+
+const UserItem = () => {
+  return <div></div>;
+};
+
+export default UserItem;
---
+++ src/user/components/usersList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../css/user/components/UsersList.css";
+
+const UsersList = (props) => {
+  return <div></div>;
+};
+
+export default UsersList;
